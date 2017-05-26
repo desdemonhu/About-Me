ffa3011daf83a87193b9b82879abb9276dd559dc
Raegan Millhollin <desdemonhu@gmail.com>
2017-05-25 19:50:04 -0700

    Basic CSS and HTML done

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 'use strict';
-
 /*
-var confirmed = confirm('Are you ready to rock and roll!?');
-
-if(confirmed === true){
-  console.log('The user is ready!');
-} else {
-  console.log('The user said they were not ready... but we\'re going to start anyway');
-}
-*/
-
 var name = prompt('Welcome Citizen. What is your Designation?');
 console.log('Mimic\'s name is: '+ name);
+
+alert('Excellent job, '+ name + ', your typing skills are superb!');
+
+var testReady = confirm('Are you ready for your calibration test?');
+
+if(testReady){
+  console.log('Your enthusiasm is appreciated. Let us begin.');
+} else{
+  console.log('I\'m sorry, ' + name + ', calibration is necessary. Let us begin.');
+}
+*/
