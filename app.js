5c6fd6331d66f129f24ff30edc938232533095ec
Raegan Millhollin <desdemonhu@gmail.com>
2017-05-25 20:43:43 -0700

    Finished the questions. The website still looks like garbage tho

--- app.js
+++ app.js
@@ -41,10 +41,36 @@ var questionGames = prompt('Does Raegan play video games? Y/N');
 if(questionGames.toUpperCase()==='Y') {
   console.log('Lots of video games');
   alert('You are correct!');
-} else if (questionCat.toUpperCase()==='N') {
+} else if (questionGames.toUpperCase()==='N') {
   console.log('NOPE! ALL THE VIDEO GAMES!');
   alert('You were almost right. Excellent job!');
 } else{
   alert('You have deviated.');
-  console.log(questionCat + ' is not Y/N');
+  console.log(questionGames + ' is not Y/N');
+}
+
+///Question 4
+var questionArt = prompt('Does Raegan doodle? Y/N');
+if(questionArt.toUpperCase()==='Y') {
+  console.log('She doodles a lot!');
+  alert('You are correct!');
+} else if (questionArt.toUpperCase() === 'N') {
+  console.log('The test is lying, I love doodling');
+  alert('She has never picked up a pencil! Good job!');
+} else{
+  alert('You have deviated.');
+  console.log(questionArt + ' is not Y/N');
+}
+
+///Question 5
+var questionTaco = prompt('Does Raegan like tacos? Y/N');
+if(questionTaco.toUpperCase()==='Y') {
+  console.log('She love a taco!');
+  alert('You are correct!');
+} else if (questionTaco.toUpperCase() === 'N') {
+  console.log('I guess I don\'t feel strongly about tacos');
+  alert('It is only food.');
+} else{
+  alert('You have deviated.');
+  console.log(questionArt + ' is not Y/N');
 }
