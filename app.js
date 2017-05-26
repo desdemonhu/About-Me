da7132f5598e689d9bb35d7de4f7062977d9a19d
Raegan Millhollin <desdemonhu@gmail.com>
2017-05-26 11:24:51 -0700

    Updated pictures and added answer variables to each question

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 ///For writing out the answers to the test
-var answerCorrect = '<strong><img src="/assets/checkmark.png">Correct</strong>';
-var answerWrong = '<strong>Incorrect</strong>'
+var answerCorrect = '<strong><img src="assets/checkmark.png" />Correct</strong>';
+var answerWrong = '<strong><img src="assets/wrong.png" />Incorrect</strong>'
 
 var name = prompt('Welcome Citizen. What is your Designation?');
 console.log('Mimic name: '+ name);
@@ -36,55 +36,71 @@ if(questionMovie.toUpperCase() === "THE EXORCIST" || questionMovie.toUpperCase()
 }
 
 ///Question 2
+var questionCatAnswer;
 var questionCat = prompt('Does Raegan have a fluffy little kitty cat? Y/N');
 if(questionCat.toUpperCase() === 'Y' || questionCat.toUpperCase() === 'YES') {
   console.log('The test is lying, there are no cats.');
+  questionCatAnswer = answerWrong;
   alert('How did you guess?');
 } else if (questionCat.toUpperCase() === 'N' || questionCat.toUpperCase() === 'NO') {
   console.log('No cats');
+  questionCatAnswer = answerCorrect;
   alert('You are correct!');
 } else{
   alert('You have deviated.');
-  console.log(questionCat + ' is not Y/N');
+  console.log(questionCat + ' is not Y/N')
+  questionCatAnswer = answerWrong;
 }
 
 ///Question 3
+var questionGamesAnswer;
 var questionGames = prompt('Does Raegan play video games? Y/N');
 if(questionGames.toUpperCase() ==='Y' || questionGames.toUpperCase() === 'YES') {
   console.log('Lots of video games');
+  questionGamesAnswer = answerCorrect;
   alert('You are correct!');
 } else if (questionGames.toUpperCase() === 'N' || questionGames.toUpperCase() === 'NO') {
   console.log('NOPE! ALL THE VIDEO GAMES!');
+  questionGamesAnswer = answerWrong;
   alert('You were almost right. Excellent job!');
 } else{
   alert('You have deviated.');
   console.log(questionGames + ' is not Y/N');
+  questionGamesAnswer = answerWrong;
 }
 
 ///Question 4
+var questionArtAnswer;
 var questionArt = prompt('Does Raegan doodle? Y/N');
 if(questionArt.toUpperCase()==='Y' || questionArt.toUpperCase() === 'YES') {
   console.log('She doodles a lot!');
+  questionArtAnswer = answerCorrect;
   alert('You are correct!');
 } else if (questionArt.toUpperCase() === 'N' || questionArt.toUpperCase() === 'NO') {
   console.log('The test is lying, I love doodling');
+  questionArtAnswer = answerWrong;
   alert('She has never picked up a pencil! Good job!');
 } else{
   alert('You have deviated.');
   console.log(questionArt + ' is not Y/N');
+  questionArtAnswer = answerWrong;
 }
 
 ///Question 5
+var questionTacoAnswer;
 var questionTaco = prompt('Does Raegan like tacos? Y/N');
 if(questionTaco.toUpperCase()==='Y' || questionTaco.toUpperCase() === 'YES') {
   console.log('She love a taco!');
+  questionTacoAnswer = answerCorrect;
   alert('You are correct!');
 } else if (questionTaco.toUpperCase() === 'N' || questionTaco.toUpperCase() === 'NO') {
   console.log('I guess I don\'t feel strongly about tacos');
+  questionTacoAnswer = answerWrong;
   alert('It is only food.');
 } else{
   alert('You have deviated.');
   console.log(questionTaco + ' is not Y/N');
+  questionTacoAnswer = answerWrong;
 }
 
 ///Write quiz answers to quiz-answers
