665058e90ca53a469621de70f2c7360c077d8022
Raegan Millhollin <desdemonhu@gmail.com>
2017-05-25 18:35:24 -0700

    Deleted stuff from class yesterday and started with my first question

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict';
 
+/*
 var confirmed = confirm('Are you ready to rock and roll!?');
 
 if(confirmed === true){
   console.log('The user is ready!');
 } else {
-  console.log('The user said the were not ready... but we\'re going to start anyway');
+  console.log('The user said they were not ready... but we\'re going to start anyway');
 }
+*/
+
+var name = prompt('Welcome Citizen. What is your Designation?');
+console.log('Mimic\'s name is: '+ name);
