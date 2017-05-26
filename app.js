8f64636c27bd781d259376f58971a85734ef4fad
Raegan Millhollin <desdemonhu@gmail.com>
2017-05-25 20:08:06 -0700

    First question is complete

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 'use strict';
-/*
+
 var name = prompt('Welcome Citizen. What is your Designation?');
-console.log('Mimic\'s name is: '+ name);
+console.log('Mimic name: '+ name);
 
 alert('Excellent job, '+ name + ', your typing skills are superb!');
 
 var testReady = confirm('Are you ready for your calibration test?');
 
 if(testReady){
-  console.log('Your enthusiasm is appreciated. Let us begin.');
+  alert('Your enthusiasm is appreciated. Let us begin.');
 } else{
-  console.log('I\'m sorry, ' + name + ', calibration is necessary. Let us begin.');
+  alert('I\'m sorry, ' + name + ', calibration is necessary. Let us begin.');
 }
-*/
+
+///Question one
+var questionMovie = prompt('What movie is Raegan\'s name from?');
+if(questionMovie.toUpperCase() === "THE EXORCIST" || questionMovie.toUpperCase() === "EXORCIST"){
+  console.log('Mimic answer: '+ questionMovie);
+  alert('You are correct!')
+} else {
+  console.log('Wow, that is so wrong.');
+  alert(questionMovie + ' is an excellent answer!');
+}
